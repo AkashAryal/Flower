cd81088445dcf02ccb9841366636623914767527
Sam Zhou <sam@developersam.com>
2020-09-23 19:12:43 -0400

    Setup a working example for NLP API (#4)
    Run `yarn compile` to compile everything and run `yarn workspace server start:nlp-example` to run a test

---
+++ server/src/nlp-process.ts
@@ -0,0 +1,21 @@
+import language from '@google-cloud/language';
+
+import { MutableDocumentInterestVector, DocumentInterestVector } from './types';
+
+const GCLOUD_NLP_CLIENT = new language.LanguageServiceClient();
+
+const computeInterestVector = async (text: string): Promise<DocumentInterestVector> => {
+  const [analysisResult] = await GCLOUD_NLP_CLIENT.analyzeEntities({
+    document: { content: text, type: 'PLAIN_TEXT' },
+  });
+  const interestVector: MutableDocumentInterestVector = {};
+  (analysisResult.entities ?? []).forEach(({ name, salience }) => {
+    if (name == null || salience == null) {
+      return;
+    }
+    interestVector[name.toLocaleLowerCase()] = salience;
+  });
+  return interestVector;
+};
+
+export default computeInterestVector;
