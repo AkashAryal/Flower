6c75f9604e501ad7d24d4821f1c9d22abed085d5
Sam <sam@developersam.com>
2020-09-23 21:49:01 -0400

    Implement basic firebase https functions with auth
    Added an unsafe mode gated by UNSAFE envvar to let us impersonate any user during dev. Might be helpful for debugging. Tested using firebase emulator locally.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
-export = {};
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp();
+
+const createHttpsFunctionWithAuthWall = (
+  handler: (userEmail: string) => Promise<unknown>
+): functions.HttpsFunction =>
+  functions.https.onRequest(
+    async (request, response): Promise<void> => {
+      const { accessToken } = request.query;
+      if (typeof accessToken !== 'string') {
+        response.status(403).send('Permission denied');
+        return;
+      }
+      if (process.env.UNSAFE === 'true') {
+        // Allow us to impersonate users with certain email during development.
+        // Treat access token as userEmail.
+        const json = await handler(accessToken);
+        response.status(200).send(json);
+      } else {
+        const decodedIDToken = await admin.auth().verifyIdToken(accessToken);
+        const userEmail = decodedIDToken.email;
+        if (userEmail == null) {
+          response.status(403).send('Permission denied');
+          return;
+        }
+        const json = await handler(userEmail);
+        response.status(200).send(json);
+      }
+    }
+  );
+
+// TODO: implement this function when we have more DB structures.
+export const getTrending = createHttpsFunctionWithAuthWall(async () => []);
+
+export const getInterested = createHttpsFunctionWithAuthWall(async () => []);
