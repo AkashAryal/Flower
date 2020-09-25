6c307a9417d9ec48ec077b797ad9dbc0e8614798
Akash Aryal <aryal.akash4414@gmail.com>
2020-09-25 17:58:24 -0500

    format

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,19 +1,18 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement } from 'react';
 
 import Link from 'next/link';
 
 import { useUser } from '../hooks/user';
 
-
 const Profile = (): ReactElement => {
   const user = useUser();
 
-  const options: { name: string, link: string }[] = [
-    { name: "Scheduled Studies", link: "/scheduled-studies" },
-    { name: "My studies", link: "/my-studies" },
-    { name: "My Profile", link: "/my-profile" },
-    { name: "IRB Eligibility", link: "/irb-eligibility" }
-  ]
+  const options: { name: string; link: string }[] = [
+    { name: 'Scheduled Studies', link: '/scheduled-studies' },
+    { name: 'My studies', link: '/my-studies' },
+    { name: 'My Profile', link: '/my-profile' },
+    { name: 'IRB Eligibility', link: '/irb-eligibility' },
+  ];
 
   return (
     <div style={{ textAlign: 'center' }}>
@@ -21,23 +20,22 @@ const Profile = (): ReactElement => {
       <img src="https://i.stack.imgur.com/34AD2.jpg" alt="profile" />
       <br />
       {
-        <div>Hello {user.displayName} <br /><br /></div>
+        <div>
+          Hello {user.displayName} <br />
+          <br />
+        </div>
       }
-      {
-        options.map(o => {
-          return (
-            <div key={o.link}>
-              <Link href={o.link} >
-                {o.name}
-              </Link>
-              <br />
-              <br />
-            </div>
-          );
-        })
-      }
+      {options.map((o) => {
+        return (
+          <div key={o.link}>
+            <Link href={o.link}>{o.name}</Link>
+            <br />
+            <br />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
