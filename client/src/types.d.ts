2714b12fbba0a03058568d469235602ad80b89c5
Sam Zhou <sam@developersam.com>
2020-09-25 19:12:44 -0400

    Allow user to update profile (#21)
    This diff implements update functionality. In the next one, I will make it pull from server if profile is already there instead of showing empty inputs.

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,8 +1,8 @@
 /**
  * Type definitions for the app that can be used without import.
  * Please reserve it only for model type definition, and prefix every type with App.
- * For all types that are coming from database, you should create two versions of it: one with ID
- * and one without ID.
+ * For all types that are coming from database with an non-email id, you should create two versions
+ * of it: one with ID and one without ID.
  */
 
 type AppUser = {
@@ -23,3 +23,13 @@ type AppStudy = {
 };
 
 type AppStudyWithoutID = Omit<AppStudy, 'id'>;
+
+type AppProfile = {
+  readonly displayName: string;
+  readonly classYear: string;
+  readonly birthday: string;
+  readonly major: string;
+  readonly selfIntroduction: readonly string;
+  readonly interests: readonly string[];
+  readonly skills: readonly string[];
+};
