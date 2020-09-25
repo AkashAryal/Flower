9fa594f22f76d96aed6b9c2523def2d8c1a077cb
Sam Zhou <sam@developersam.com>
2020-09-25 13:14:51 -0400

    Setup a dummy endpoint showing how to use access token from firebase (#12)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,32 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 
+import firebase from '../fbConfig';
 import PrivateComponent from '../hoc/PrivateComponent';
 
 const HomePage = (): ReactElement => {
+  const [basicUserInformation, setBasicUserInformation] = useState<
+    { readonly name: string; readonly email: string } | undefined
+  >();
+
+  useEffect(() => {
+    firebase
+      .functions()
+      .httpsCallable('getUserNameForTesting')()
+      .then((r) => setBasicUserInformation(r.data));
+  }, []);
+
   return (
     <PrivateComponent>
-      <div>Home Page</div>
+      <>
+        <div>Home Page</div>
+        {basicUserInformation == null ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            You are {basicUserInformation.name} with email: {basicUserInformation.email}.
+          </div>
+        )}
+      </>
     </PrivateComponent>
   );
 };
