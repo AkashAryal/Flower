77b0e3f879e26146a38b570ddccc00f7a75aa873
Sam Zhou <sam@developersam.com>
2020-09-26 18:21:06 -0400

    Make studies page the index page (#51)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,73 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 
+import LoadingRing from '../components/LoadingRing';
+import { SchedulableStudyCard, EditableStudyCard } from '../components/StudyCard';
 import firebase from '../fbConfig';
 import { useUser } from '../hooks/user';
+import styles from './studies.module.css';
 
-const HomePage = (): ReactElement => {
-  const user = useUser();
+const StudiesPage = (): ReactElement => {
+  const { email: owner } = useUser();
+  const [trendingStudies, setTrendingStudies] = useState<
+    readonly AppStudyWithOccupiedTimes[] | undefined
+  >();
+  const [interestedStudies, setInterestedStudies] = useState<
+    readonly AppStudyWithOccupiedTimes[] | undefined
+  >();
+  const [myStudies, setMyStudies] = useState<readonly AppStudy[] | undefined>();
+
+  useEffect(() => {
+    firebase
+      .functions()
+      .httpsCallable('getTrending')()
+      .then((r) => setTrendingStudies(r.data));
+    firebase
+      .functions()
+      .httpsCallable('getInterested')()
+      .then((r) => setInterestedStudies(r.data));
+    firebase
+      .firestore()
+      .collection('studies')
+      .where('owner', '==', owner)
+      .onSnapshot((snapshot) =>
+        setMyStudies(snapshot.docs.map((it) => ({ ...it.data(), id: it.id } as AppStudy)))
+      );
+  }, [owner]);
 
   return (
     <div>
-      <div>Home Page</div>
-      <button onClick={() => firebase.auth().signOut()}>Log out</button>
-      <div>
-        You are {user.displayName} with email: {user.email}.
-      </div>
+      <h2 className={styles.StudiesSectionHeader}>Trending</h2>
+      {trendingStudies ? (
+        <div className={styles.StudiesContainer}>
+          {trendingStudies.map((study) => (
+            <SchedulableStudyCard key={study.id} study={study} />
+          ))}
+        </div>
+      ) : (
+        <LoadingRing />
+      )}
+      <h2 className={styles.StudiesSectionHeader}>You may be interested in</h2>
+      {interestedStudies ? (
+        <div className={styles.StudiesContainer}>
+          {interestedStudies.map((study) => (
+            <SchedulableStudyCard key={study.id} study={study} />
+          ))}
+        </div>
+      ) : (
+        <LoadingRing />
+      )}
+      <h2 className={styles.StudiesSectionHeader}>My Studies</h2>
+      {myStudies ? (
+        <div className={styles.StudiesContainer}>
+          {myStudies.map((study) => (
+            <EditableStudyCard key={study.id} study={study} />
+          ))}
+        </div>
+      ) : (
+        <LoadingRing />
+      )}
     </div>
   );
 };
 
-export default HomePage;
+export default StudiesPage;
