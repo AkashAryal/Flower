adf74ceebfd2f0de73ff666faa393ba0e714bd50
Akash Aryal <aryal.akash4414@gmail.com>
2020-09-25 19:09:24 -0500

    basic study page that lists all entried in studies db

---
+++ client/src/components/Study.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from 'react';
+
+type P = {
+  study: AppStudyWithoutID
+}
+
+const Study = (props: P): ReactElement => {
+  return (
+    <div>
+      <img src="https://i.imgur.com/SR0YwEv.jpg" alt="study" />
+      <p style={{ fontWeight: "bolder" }}>{props.study.projectName}</p>
+      <p>Rewards: {props.study.rewards}</p>
+      <p>Description: {props.study.description}</p>
+      <p>Description: {props.study.avaliableTimes}</p>
+      <p>Duration: {props.study.duration}</p>
+      <p>Eligibility: {props.study.eligibility}</p>
+      <p>Owner: {props.study.owner} </p>
+    </div>
+  )
+}
+
+export default Study;
---
+++ client/src/pages/studies.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { ReactElement, useEffect, useState } from 'react';
+
+import Study from "../components/Study";
+import firebase from '../fbConfig';
+
+
+const Studies = (): ReactElement => {
+  const [studies, setStudies] = useState<AppStudyWithoutID[]>([]);
+
+  useEffect(() => {
+    getAllStudies();
+  }, []);
+
+  const getAllStudies = (): void => {
+    firebase.firestore().collection("studies").onSnapshot((doc) => {
+      const data: AppStudyWithoutID[] = [];
+      doc.forEach((d: any) =>
+        data.push(d.data())
+      );
+      console.log("arryof data", data);
+
+      setStudies(data);
+      console.log("studies", studies);
+    })
+  };
+
+  // TODO: trending
+  const getTrending = () => {
+    return studies
+  }
+
+  // TODO: trending
+  const getRecommended = () => {
+    return studies;
+  }
+
+  return (
+    <div>
+      TOP BAR
+      <br />
+      <p style={{ fontWeight: "bolder" }}>Trending</p><br />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        {
+          getTrending().map((study: AppStudyWithoutID) => {
+            return (
+              <div key={study.projectName} >
+                <Study study={study} />
+              </div>
+            )
+          })
+        }
+      </div>
+      <br /><br />
+
+      <p style={{ fontWeight: "bolder" }}>You may be interested in</p><br />
+      {
+        getRecommended().map((study: AppStudyWithoutID) => {
+          return (
+            <div key={study.projectName} >
+              <Study study={study} />
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Studies;
