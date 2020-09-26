7f84ce9e559d0424df9f4d22edb20b6b88f2ecb5
Sam <sam@developersam.com>
2020-09-26 12:12:19 -0400

    Implement editable my studies section

--- client/src/components/StudyCardInlineEditor.tsx
+++ client/src/components/StudyCardInlineEditor.tsx
@@ -1,7 +1,25 @@
 import React, { ReactElement } from 'react';
 
-const StudyCardInlineEditor = (): ReactElement => {
-  return <div className="card__body">TODO StudyCardInlineEditor</div>;
+import firebase from '../fbConfig';
+import StudyEditor from './StudyEditor';
+
+type Props = {
+  readonly study: AppStudy;
+  readonly onSubmitted: () => void;
+};
+
+const StudyCardInlineEditor = ({ study, onSubmitted }: Props): ReactElement => {
+  const onSubmit = (appStudyWithoutOwner: AppStudyWithoutIDOwner): void => {
+    const appStudy: AppStudyWithoutID = { owner: study.owner, ...appStudyWithoutOwner };
+    firebase.firestore().collection('studies').doc(study.id).set(appStudy);
+    onSubmitted();
+  };
+
+  return (
+    <div className="card__body">
+      <StudyEditor initialStudy={study} submitButtonName="Update" onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default StudyCardInlineEditor;
