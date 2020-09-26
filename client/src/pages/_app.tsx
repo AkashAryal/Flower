069762e44f53ec88a705e7cd723b186da30efe73
Sam <sam@developersam.com>
2020-09-25 22:34:41 -0400

    Use infima css to build navbar
    Infima css powers classic theme of docusaurus. My personal website also uses it. I think it's good enough.

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React, { ReactElement } from 'react';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Link from 'next/link';
 import { RecoilRoot } from 'recoil';
 
 import PrivateComponent from '../components/PrivateComponent';
+
+import 'infima/dist/css/default/default.min.css';
+import './index.css';
 
 const App = (props: AppProps): ReactElement => {
   const { Component, pageProps } = props;
@@ -18,7 +24,26 @@ const App = (props: AppProps): ReactElement => {
       </Head>
       <RecoilRoot>
         <PrivateComponent>
-          <Component {...pageProps} />
+          <>
+            <nav className="navbar">
+              <div className="navbar__inner">
+                <div className="navbar__items">
+                  <Link href="/">
+                    <a className="navbar__brand">Flower</a>
+                  </Link>
+                </div>
+                <div className="navbar__items navbar__items--right">
+                  <Link href="/create-study">
+                    <a className="navbar__brand">Create Study</a>
+                  </Link>
+                  <Link href="/profile">
+                    <a className="navbar__brand">Profile</a>
+                  </Link>
+                </div>
+              </div>
+            </nav>
+            <Component {...pageProps} />
+          </>
         </PrivateComponent>
       </RecoilRoot>
     </>
