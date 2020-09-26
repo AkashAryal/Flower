a4017161f030099f51af4948afe4eb467a8c7f8f
Akash Aryal <aryal.akash4414@gmail.com>
2020-09-25 19:11:06 -0500

    linter

--- client/src/components/Study.tsx
+++ client/src/components/Study.tsx
@@ -1,14 +1,14 @@
 import React, { ReactElement } from 'react';
 
 type P = {
-  study: AppStudyWithoutID
-}
+  study: AppStudyWithoutID;
+};
 
 const Study = (props: P): ReactElement => {
   return (
     <div>
       <img src="https://i.imgur.com/SR0YwEv.jpg" alt="study" />
-      <p style={{ fontWeight: "bolder" }}>{props.study.projectName}</p>
+      <p style={{ fontWeight: 'bolder' }}>{props.study.projectName}</p>
       <p>Rewards: {props.study.rewards}</p>
       <p>Description: {props.study.description}</p>
       <p>Description: {props.study.avaliableTimes}</p>
@@ -16,7 +16,7 @@ const Study = (props: P): ReactElement => {
       <p>Eligibility: {props.study.eligibility}</p>
       <p>Owner: {props.study.owner} </p>
     </div>
-  )
-}
+  );
+};
 
 export default Study;
--- client/src/pages/studies.tsx
+++ client/src/pages/studies.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { ReactElement, useEffect, useState } from 'react';
 
-import Study from "../components/Study";
+import Study from '../components/Study';
 import firebase from '../fbConfig';
-
 
 const Studies = (): ReactElement => {
   const [studies, setStudies] = useState<AppStudyWithoutID[]>([]);
@@ -13,58 +12,55 @@ const Studies = (): ReactElement => {
   }, []);
 
   const getAllStudies = (): void => {
-    firebase.firestore().collection("studies").onSnapshot((doc) => {
-      const data: AppStudyWithoutID[] = [];
-      doc.forEach((d: any) =>
-        data.push(d.data())
-      );
-      console.log("arryof data", data);
-
-      setStudies(data);
-      console.log("studies", studies);
-    })
+    firebase
+      .firestore()
+      .collection('studies')
+      .onSnapshot((doc) => {
+        const data: AppStudyWithoutID[] = [];
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        doc.forEach((d: any) => data.push(d.data()));
+        setStudies(data);
+      });
   };
 
   // TODO: trending
   const getTrending = () => {
-    return studies
-  }
+    return studies;
+  };
 
   // TODO: trending
   const getRecommended = () => {
     return studies;
-  }
+  };
 
   return (
     <div>
       TOP BAR
       <br />
-      <p style={{ fontWeight: "bolder" }}>Trending</p><br />
-      <div style={{ display: "flex", flexDirection: "row" }}>
-        {
-          getTrending().map((study: AppStudyWithoutID) => {
-            return (
-              <div key={study.projectName} >
-                <Study study={study} />
-              </div>
-            )
-          })
-        }
-      </div>
-      <br /><br />
-
-      <p style={{ fontWeight: "bolder" }}>You may be interested in</p><br />
-      {
-        getRecommended().map((study: AppStudyWithoutID) => {
+      <p style={{ fontWeight: 'bolder' }}>Trending</p>
+      <br />
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        {getTrending().map((study: AppStudyWithoutID) => {
           return (
-            <div key={study.projectName} >
+            <div key={study.projectName}>
               <Study study={study} />
             </div>
-          )
-        })
-      }
+          );
+        })}
+      </div>
+      <br />
+      <br />
+      <p style={{ fontWeight: 'bolder' }}>You may be interested in</p>
+      <br />
+      {getRecommended().map((study: AppStudyWithoutID) => {
+        return (
+          <div key={study.projectName}>
+            <Study study={study} />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Studies;
